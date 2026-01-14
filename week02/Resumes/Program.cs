a6c795e4cab5a9610d4709d02d810e42566d6e3c
Emmanuel Ademuyiwa <samadeimmanuel@yahoo.com>
2026-01-14 10:59:10 +0100

    Week 2 Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Resume instance
+        Resume myResume = new Resume();
+        myResume._personName = "Emmanuel Ademuyiwa";
+
+        // Add jobs to the resume's job list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine($"First job title: {myResume._jobs[0]._jobTitle}");
+
+        Console.WriteLine("\nComplete Resume:");
+        myResume.Display();
     }
 }
